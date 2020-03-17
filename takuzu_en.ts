5b08f6c79f7406fe96c0d475c02521ef00e353bb
Cédric dezobry <cedric.dezobry@gmail.com>
2020-03-17 14:30:13 +0100

    add lacking files

---
+++ takuzu_en.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>QPushButton</name>
+    <message>
+        <location filename="main.cpp" line="15"/>
+        <source>Hello world!</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presenter</name>
+    <message>
+        <location filename="vue.ui" line="14"/>
+        <source>presenter</source>
+        <translation>Takuzu</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="24"/>
+        <source>Hello World!</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
