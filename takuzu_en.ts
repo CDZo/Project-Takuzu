e8e3613e8f1fa52560dd19186b73f0c582551454
Cédric dezobry <cedric.dezobry@gmail.com>
2020-03-18 10:12:57 +0100

    add new french translation

--- takuzu_en.ts
+++ takuzu_en.ts
@@ -14,13 +14,48 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="vue.ui" line="52"/>
+        <location filename="vue.ui" line="67"/>
         <source>&amp;Save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="vue.ui" line="55"/>
+        <location filename="vue.ui" line="70"/>
         <source>Save your current grid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="73"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="78"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="81"/>
+        <source>Open a grid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="84"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="89"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="92"/>
+        <source>Create a new grid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="95"/>
+        <source>Ctrl+N</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- takuzu_fr.ts
+++ takuzu_fr.ts
@@ -21,14 +21,49 @@
         <translation>Essai!</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="52"/>
+        <location filename="vue.ui" line="67"/>
         <source>&amp;Save</source>
         <translation>&amp;Enregistrer</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="55"/>
+        <location filename="vue.ui" line="70"/>
         <source>Save your current grid</source>
         <translation>Enregistrer votre grille</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="73"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="78"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="81"/>
+        <source>Open a grid</source>
+        <translation>Ouvre une grille</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="84"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="89"/>
+        <source>&amp;New</source>
+        <translation>&amp;Nouvelle</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="92"/>
+        <source>Create a new grid</source>
+        <translation>Créer une nouvelle grille</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="95"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
     </message>
 </context>
 </TS>
