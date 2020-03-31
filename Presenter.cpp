6780bf064c2490289d36d45ab3fd95b86306f478
Cédric dezobry <cedric.dezobry@gmail.com>
2020-03-31 10:12:26 +0200

    add Presenter

---
+++ Presenter.cpp
@@ -0,0 +1,6 @@
+#include "Presenter.h"
+
+Presenter::Presenter()
+{
+
+}
---
+++ Presenter.h
@@ -0,0 +1,11 @@
+#ifndef PRESENTER_H
+#define PRESENTER_H
+
+
+class Presenter
+{
+public:
+    Presenter();
+};
+
+#endif // PRESENTER_H
