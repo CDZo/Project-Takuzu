8423398b74e4b11777e9a5276726fb4d12b56669
Cédric dezobry <cedric.dezobry@gmail.com>
2020-03-31 14:43:07 +0200

    add connection between slots and signals

--- Presenter.cpp
+++ Presenter.cpp
@@ -5,8 +5,6 @@
 Presenter::Presenter()
 {
     _size = 4;
-    _model = new ModelTakuzu;
-    _model->initGrid(_size);
     _visualPawns = new Pawn[_size*_size];
 
     _visualPawns[0].setState(Black);
@@ -16,11 +14,21 @@ Presenter::Presenter()
 
     _view = new View;
     _view->loadPawnsOnGrid(_visualPawns,_size);
+
+    _model = new ModelTakuzu;
+    _model->initGrid(_size,_visualPawns);
+
     for(int i = 0; i < _size*_size;i++) {
         connect(&_visualPawns[i],SIGNAL(onClicked(int,State)),this,SLOT(onPawnClicked(int, State)));
         _visualPawns[i].setId(i);
     }
     connect(this,SIGNAL(pawnChanged(int, State)),_model,SLOT(onPawnChanged(int, State)));
+    connect(_model,SIGNAL(incorrectPawnsInRow(std::set<std::pair<int,int>>)),this,SLOT(onIncorrectPawnsInRow(std::set<std::pair<int,int>>)));
+    connect(_model,SIGNAL(incorrectPawnsInColumn(std::set<std::pair<int,int>>)),this,SLOT(onIncorrectPawnsInColumn(std::set<std::pair<int,int>>)));
+    connect(_model,SIGNAL(unbalancedRows(std::set<int>)),this,SLOT(onUnbalancedRows(std::set<int>)));
+    connect(_model,SIGNAL(unbalancedColumns(std::set<int>)),this,SLOT(onUnbalancedColumns(std::set<int>)));
+    connect(_model,SIGNAL(identicalRows(std::set<std::pair<int,int>>)),this,SLOT(onIdenticalRows(std::set<std::pair<int,int>>)));
+    connect(_model,SIGNAL(identicalColumns(std::set<std::pair<int,int>>)),this,SLOT(onIdenticalColumns(std::set<std::pair<int,int>>)));
 }
 
 
@@ -70,8 +78,68 @@ void Presenter::show()
     _view->show();
 }
 
+void Presenter::resetFalsePawns()
+{
+    for(int i = 0; i< _size*_size;i++) {
+        _visualPawns[i].setFalse(false);
+    }
+    _view->update();
+}
+
 void Presenter::onPawnClicked(const int & id, const State & state)
 {
-    std::cout << "id: "<<id<<" state : "<<state <<std::endl;
+    //std::cout << "id: "<<id<<" state : "<<state <<std::endl;
+    resetFalsePawns();
     emit pawnChanged(id, state);
 }
+
+void Presenter::onIncorrectPawnsInRow(const std::set<std::pair<int, int>> pawns)
+{
+    for(std::set<std::pair<int, int>>::iterator it = pawns.begin();it != pawns.end();it++) {
+        _visualPawns[it->first * _size + it->second].setFalse(true);
+        //std::cout << "x: " << it->first << " y : " << it->second <<std::endl;
+    }
+    _view->update();
+}
+
+void Presenter::onIncorrectPawnsInColumn(const std::set<std::pair<int, int> > pawns)
+{
+    for(std::set<std::pair<int, int>>::iterator it = pawns.begin();it != pawns.end();it++) {
+        _visualPawns[it->first * _size + it->second].setFalse(true);
+        //std::cout << "x: " << it->first << " y : " << it->second <<std::endl;
+    }
+    _view->update();
+}
+
+void Presenter::onUnbalancedRows(std::set<int> rows)
+{
+    for(std::set<int>::iterator it = rows.begin();it != rows.end();it++) {
+     //   std::cout << "unbalanced row: " << *it <<std::endl;
+    }
+    std::cout<<std::endl;
+}
+
+void Presenter::onUnbalancedColumns(std::set<int> columns)
+{
+    for(std::set<int>::iterator it = columns.begin();it != columns.end();it++) {
+       // std::cout << "unbalanced columns: " << *it <<std::endl;
+    }
+    std::cout<<std::endl;
+}
+
+void Presenter::onIdenticalRows(std::set<std::pair<int, int> > rows)
+{
+    std::cout<<"Identical Rows :"<<std::endl;
+    for(std::set<std::pair<int,int>>::iterator it = rows.begin();it != rows.end();it++) {
+        std::cout << it->first <<" - " <<it->second <<std::endl;
+    }
+}
+
+void Presenter::onIdenticalColumns(std::set<std::pair<int, int> > columns)
+{
+    std::cout<<"Identical columns :"<<std::endl;
+    for(std::set<std::pair<int,int>>::iterator it = columns.begin();it != columns.end();it++) {
+        std::cout << it->first <<" - " <<it->second <<std::endl;
+    }
+}
+
--- Presenter.h
+++ Presenter.h
@@ -12,12 +12,21 @@ public:
     void createGrid(const int & size, const int & difficulty);
     void show();
 private:
+    void resetFalsePawns();
     View *_view;
     ModelTakuzu *_model;
     Pawn *_visualPawns;
     int _size = 0;
 public slots:
     void onPawnClicked(const int & id, const State & state);
+    void onIncorrectPawnsInRow(const std::set<std::pair<int,int>> pawns);
+    void onIncorrectPawnsInColumn(const std::set<std::pair<int,int>> pawns);
+    void onUnbalancedRows(std::set<int> rows);
+    void onUnbalancedColumns(std::set<int> columns);
+    void onIdenticalRows(std::set<std::pair<int,int>> rows);
+    void onIdenticalColumns(std::set<std::pair<int,int>> columns);
+
+
 signals:
     void pawnChanged(const int & id, const State & state);
 };
