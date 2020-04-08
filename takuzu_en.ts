d391bf3c833c38591d542d178fff97959a63f4ff
Cédric dezobry <cedric.dezobry@gmail.com>
2020-04-08 10:14:09 +0200

    update translation

--- takuzu_en.ts
+++ takuzu_en.ts
@@ -2,56 +2,104 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
-    <name>presenter</name>
+    <name>View</name>
     <message>
-        <location filename="vue.ui" line="14"/>
+        <location filename="vue.ui" line="20"/>
         <source>presenter</source>
         <translation>Takuzu</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="72"/>
+        <location filename="vue.ui" line="70"/>
         <source>&amp;Save</source>
         <translation>&amp;Save</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="75"/>
+        <location filename="vue.ui" line="73"/>
         <source>Save your current grid</source>
         <translation>Save your current grid</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="78"/>
+        <location filename="vue.ui" line="76"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="83"/>
+        <location filename="vue.ui" line="81"/>
         <source>&amp;Open</source>
         <translation>&amp;Open</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="86"/>
+        <location filename="vue.ui" line="84"/>
         <source>Open a grid</source>
         <translation>Open a grid</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="89"/>
+        <location filename="vue.ui" line="87"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="94"/>
+        <location filename="vue.ui" line="92"/>
         <source>&amp;New</source>
         <translation>&amp;New</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="97"/>
+        <location filename="vue.ui" line="95"/>
         <source>Create a new grid</source>
         <translation>Create a new grid</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="100"/>
+        <location filename="vue.ui" line="98"/>
         <source>Ctrl+N</source>
         <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="103"/>
+        <source>&amp;Option</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presenter</name>
+    <message>
+        <source>presenter</source>
+        <translation type="vanished">Takuzu</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation type="vanished">&amp;Save</translation>
+    </message>
+    <message>
+        <source>Save your current grid</source>
+        <translation type="vanished">Save your current grid</translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation type="vanished">Ctrl+S</translation>
+    </message>
+    <message>
+        <source>&amp;Open</source>
+        <translation type="vanished">&amp;Open</translation>
+    </message>
+    <message>
+        <source>Open a grid</source>
+        <translation type="vanished">Open a grid</translation>
+    </message>
+    <message>
+        <source>Ctrl+O</source>
+        <translation type="vanished">Ctrl+O</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="vanished">&amp;New</translation>
+    </message>
+    <message>
+        <source>Create a new grid</source>
+        <translation type="vanished">Create a new grid</translation>
+    </message>
+    <message>
+        <source>Ctrl+N</source>
+        <translation type="vanished">Ctrl+N</translation>
     </message>
 </context>
 </TS>
--- takuzu_fr.ts
+++ takuzu_fr.ts
@@ -9,60 +9,96 @@
     </message>
 </context>
 <context>
-    <name>presenter</name>
+    <name>View</name>
     <message>
-        <location filename="vue.ui" line="14"/>
+        <location filename="vue.ui" line="20"/>
         <source>presenter</source>
         <translation>Takuzu</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="70"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="73"/>
+        <source>Save your current grid</source>
+        <translation>Enregistrer votre grille</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="76"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="81"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="84"/>
+        <source>Open a grid</source>
+        <translation>Ouvre une grille</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="87"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="92"/>
+        <source>&amp;New</source>
+        <translation>&amp;Nouvelle</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="95"/>
+        <source>Create a new grid</source>
+        <translation>Créer une nouvelle grille</translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="98"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="vue.ui" line="103"/>
+        <source>&amp;Option</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presenter</name>
+    <message>
+        <source>presenter</source>
+        <translation type="vanished">Takuzu</translation>
     </message>
     <message>
         <source>Hello World!</source>
         <translation type="vanished">Essai!</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="72"/>
         <source>&amp;Save</source>
-        <translation>&amp;Enregistrer</translation>
+        <translation type="vanished">&amp;Enregistrer</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="75"/>
         <source>Save your current grid</source>
-        <translation>Enregistrer votre grille</translation>
+        <translation type="vanished">Enregistrer votre grille</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="78"/>
-        <source>Ctrl+S</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="vue.ui" line="83"/>
         <source>&amp;Open</source>
-        <translation>&amp;Ouvrir</translation>
+        <translation type="vanished">&amp;Ouvrir</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="86"/>
         <source>Open a grid</source>
-        <translation>Ouvre une grille</translation>
+        <translation type="vanished">Ouvre une grille</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="89"/>
-        <source>Ctrl+O</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="vue.ui" line="94"/>
         <source>&amp;New</source>
-        <translation>&amp;Nouvelle</translation>
+        <translation type="vanished">&amp;Nouvelle</translation>
     </message>
     <message>
-        <location filename="vue.ui" line="97"/>
         <source>Create a new grid</source>
-        <translation>Créer une nouvelle grille</translation>
-    </message>
-    <message>
-        <location filename="vue.ui" line="100"/>
-        <source>Ctrl+N</source>
-        <translation></translation>
+        <translation type="vanished">Créer une nouvelle grille</translation>
     </message>
 </context>
 </TS>
